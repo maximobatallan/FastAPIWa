c08715b9cea7de4f4fd0b43b4feede3bacb0808b
maximobatallan <71332822+maximobatallan@users.noreply.github.com>
2023-09-23 03:54:57 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+import jsonify
+import requests
+
+class Item(BaseModel):
+    name: str
+    description: str 
+    price: float
+    tax: float 
+
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"greeting": "Hello, World!", "message": "Welcome to FastAPI!"}
+
+@app.post("https://graph.facebook.com/v17.0/137446296107512/messages")
+async def create_item(item: Item):
+    #SI HAY DATOS RECIBIDOS VIA GET
+    if requests.method == "GET":
+        #SI EL TOKEN ES IGUAL AL QUE RECIBIMOS
+        if requests.args.get('hub.verify_token') == "River":
+            #ESCRIBIMOS EN EL NAVEGADOR EL VALOR DEL RETO RECIBIDO DESDE FACEBOOK
+            return requests.args.get('hub.challenge')
+        else:
+            #SI NO SON IGUALES RETORNAMOS UN MENSAJE DE ERROR
+          return "Error de autentificacion."
+    #RECIBIMOS TODOS LOS DATOS ENVIADO VIA JSON
+    data=requests.get_json()
+    
+    return jsonify({"status": "success"}, 200)
+
+
