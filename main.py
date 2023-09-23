d8410c5fe0355597a12a7e1aceca632ff8f71398
maximobatallan <71332822+maximobatallan@users.noreply.github.com>
2023-09-23 04:17:50 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,17 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
-import jsonify
-import requests
+import fastapi
 
-class Item(BaseModel):
-    name: str
-    description: str 
-    price: float
-    tax: float 
+app = fastapi.FastAPI()
 
+@app.get("/webhooks")
+async def verify():
+    # Verifica el token de verificación
 
-app = FastAPI()
+    if request.query_params.get("hub.challenge") != "1158201444":
+        return Response(content="El desafío no es correcto", status_code=403)
 
+    if request.query_params.get("hub.verify_token") != "wh":
+        return Response(content="El token de verificación no es correcto", status_code=403)
 
+    # Devuelve el desafío
 
-@app.route("/webhook/", methods=["POST","GET"])
-async def create_item(item: Item):
-    #SI HAY DATOS RECIBIDOS VIA GET
-    if requests.method == "GET":
-        #SI EL TOKEN ES IGUAL AL QUE RECIBIMOS
-        if requests.args.get('hub.verify_token') == "HolaNovato":
-            #ESCRIBIMOS EN EL NAVEGADOR EL VALOR DEL RETO RECIBIDO DESDE FACEBOOK
-            return requests.args.get('hub.challenge')
-        else:
-            #SI NO SON IGUALES RETORNAMOS UN MENSAJE DE ERROR
-          return "Error de autentificacion."
-    #RECIBIMOS TODOS LOS DATOS ENVIADO VIA JSON
-    data=requests.get_json()
-    
-    return jsonify({data}, 200)
-
-
+    return Response(content="1158201444", status_code=200)
